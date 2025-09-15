e12f55e64c5977b38b0bc1805357fac1ca065acc
Folovich <Foll.Folovich@yandex.ru>
2025-09-15 22:57:36 +0300

    42-comit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <SFML/Graphics.hpp>
+ 
+int main()
+{
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode({1000, 600}), "Follend-42");
+
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        while (const std::optional event = window.pollEvent())
+        {
+            // Close window: exit
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+ 
+        // Clear screen
+        window.clear();
+        // Update the window
+        window.display();
+    }
+}
